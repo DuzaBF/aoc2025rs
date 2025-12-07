83abf4e072235503ddca1e4b6b1019c828bf9235
Airat Galiamov <33791465+DuzaBF@users.noreply.github.com>
2025-12-08 00:10:06 +0100

    Day 7 part 2 solution

--- src/day7.rs
+++ src/day7.rs
@@ -1,42 +1,63 @@
+use core::num;
+use std::fmt::Display;
+
 use crate::read_lines::read_lines;
 
-fn update_line(first: &Vec<char>, second: &mut Vec<char>, sum: &mut usize) {
-    let mut pass: Vec<usize> = vec![];
-    let mut split: Vec<usize> = vec![];
-    {
-        for (i, ch) in second.iter().enumerate() {
-            if first[i] == '|' || first[i] == 'S' {
-                if *ch == '^' {
-                    split.push(i);
-                } else {
-                    pass.push(i);
-                }
+fn convert(grid: &Vec<Vec<char>>) -> Vec<Vec<i64>> {
+    let mut num_grid = vec![vec![]; grid.len()];
+    for i in 0..grid.len() {
+        num_grid[i].resize(grid[i].len(), 0);
+        for j in 0..grid[i].len() {
+            if grid[i][j] == 'S' {
+                num_grid[i][j] = 1;
+            }
+            if grid[i][j] == '^' {
+                num_grid[i][j] = -1;
             }
         }
+    }
+    num_grid
+}
+
+fn update_line(first: &Vec<i64>, second: &mut Vec<i64>, sum: &mut usize) {
+    let mut pass: Vec<(usize, i64)> = vec![];
+    let mut split: Vec<(usize, i64)> = vec![];
+
+    for (i, ch) in second.iter().enumerate() {
+        if first[i] > 0 {
+            if *ch == -1 {
+                split.push((i, first[i]));
+            } else {
+                pass.push((i, first[i]));
+            }
+        }
+    }
+
+    for (pos, val) in pass {
+        second[pos] += val;
     }
 
     *sum = *sum + split.len();
-    for pos in split {
+    for (pos, val) in split {
         if pos > 0 {
-            if second[pos - 1] != '^' {
-                second[pos - 1] = '|'
+            if second[pos - 1] != -1 {
+                second[pos - 1] += val;
             }
         }
         if pos < second.len() - 1 {
-            if second[pos + 1] != '^' {
-                second[pos + 1] = '|'
+            if second[pos + 1] != -1 {
+                second[pos + 1] += val;
             }
         }
     }
-
-    for pos in pass {
-        second[pos] = '|';
-    }
 }
 
-fn print(grid: &Vec<Vec<char>>) {
+fn print<T: Display>(grid: &Vec<Vec<T>>) {
     for line in grid {
-        println!("{line:?}");
+        for ch in line {
+            print!("{ch:4}")
+        }
+        println!();
     }
 }
 
@@ -50,14 +71,18 @@ pub fn solution() {
         }
     }
 
+    let mut num_grid = convert(&grid);
+    println!("---- num ----");
+    print(&num_grid);
     for i in 0..grid.len() - 1 {
         // println!("---- step {i} ----");
         // print(&grid);
-        let pair = grid.split_at_mut(i + 1);
+        let pair = num_grid.split_at_mut(i + 1);
         update_line(&pair.0[i], &mut pair.1[0], &mut sum);
     }
 
     println!("---- final ----");
-    print(&grid);
+    print(&num_grid);
     println!("sum {}", sum);
+    println!("timelines {}", num_grid.last().unwrap().iter().sum::<i64>());
 }
