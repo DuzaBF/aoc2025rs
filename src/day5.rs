972d791665522dbf1bcc292b42cb36fe815b8b5c
Airat Galiamov <33791465+DuzaBF@users.noreply.github.com>
2025-12-07 20:13:16 +0100

    Day 5 part 2 solution

--- src/day5.rs
+++ src/day5.rs
@@ -1,10 +1,38 @@
 use crate::read_lines::read_lines;
 
+fn overlap(a: &(u64, u64), b: &(u64, u64)) -> bool {
+    if a.0 < b.0 {
+        return a.1 >= b.0;
+    } else {
+        return b.1 >= a.0;
+    }
+}
+
+fn merge_ranges(ranges: &mut Vec<(u64, u64)>) -> bool {
+    let mut has_overlap = false;
+    for i in 0..ranges.len() {
+        for j in i + 1..ranges.len() {
+            if overlap(&ranges[i], &ranges[j]) {
+                // println!(
+                //     "overlap r[{:3}] = {:16?} and r[{:3}] = {:16?}",
+                //     i, ranges[i], j, ranges[j]
+                // );
+                has_overlap = true;
+                ranges[i] = (ranges[i].0.min(ranges[j].0), ranges[i].1.max(ranges[j].1));
+                // println!("new r[{:3}] = {:16?}", i, ranges[i]);
+                ranges.remove(j);
+                return has_overlap;
+            }
+        }
+    }
+    has_overlap
+}
+
 pub fn solution() {
     let mut sum = 0;
     let mut ranges: Vec<(u64, u64)> = vec![];
-    let mut ids: Vec<(u64)> = vec![];
-    let mut spoiled: Vec<(u64)> = vec![];
+    let mut ids: Vec<u64> = vec![];
+    let mut spoiled: Vec<u64> = vec![];
     let mut divider = false;
     if let Ok(lines) = read_lines("./input/day5/input") {
         for (i, line) in lines.map_while(Result::ok).into_iter().enumerate() {
@@ -23,19 +51,17 @@ pub fn solution() {
         }
     }
 
-    for id in &ids {
-        let mut spoil = true;
-        for range in &ranges {
-            if *id >= range.0 && *id <= range.1 {
-                spoil = false;
-            }
-        }
-        if !spoil {
-            spoiled.push(*id);
-        }
-    }
+    while merge_ranges(&mut ranges) {}
 
-    sum += spoiled.len();
+    ranges.sort_by(|a, b| a.0.cmp(&b.0));
+
+    let mut i = 0;
+    for r in &ranges {
+        let count = r.1 - r.0 + 1;
+        // println!("got r[{:3}] = {:16?}; count {}", i, r, count);
+        i += 1;
+        sum += count;
+    }
 
     println!("sum {}", sum);
 }
