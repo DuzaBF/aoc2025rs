730459a8948c0cf0baaed48c9eaf2e855532e203
Airat Galiamov <33791465+DuzaBF@users.noreply.github.com>
2025-12-07 17:44:16 +0100

    Day 3 part 2 solution

--- src/day3.rs
+++ src/day3.rs
@@ -1,39 +1,45 @@
 use crate::read_lines::read_lines;
 
-fn get_jolt(line: &str) -> u32 {
-    let mut max: u32 = 0;
-    let mut max_pos: usize = 0;
-    for (i, ch) in line.chars().into_iter().enumerate() {
-        if i == line.len() - 1 {
-            continue;
-        }
-        let num =  ch.to_digit(10).unwrap();
-        if num > max {
-            max = num;
-            max_pos = i;
-        }
+fn get_jolt(line: &str) -> u64 {
+    let mut pos_list: Vec<usize> = vec![];
+    let mut jolts: Vec<u32> = vec![];
+    for ch in line.chars().into_iter() {
+        let num = ch.to_digit(10).unwrap();
+        jolts.push(num);
     }
-    let mut sec: u32 = 0;
-    let mut sec_pos: usize = 0;
-    for (i, ch) in line.chars().into_iter().enumerate() {
-        if i <= max_pos {
-            continue;
-        }
-        let num =  ch.to_digit(10).unwrap();
-        if num > sec {
-            sec = num;
-            sec_pos = i;
-        }
+    let count = 12;
+    for l in 0..count {
+        let split = jolts.len() - (count - l - 1);
+        let start = match pos_list.last() {
+            Some(x) => x + 1,
+            _ => 0,
+        };
+
+        let max = *jolts[start..split].iter().max().unwrap();
+        let pos = jolts[start..split]
+            .iter()
+            .enumerate()
+            .find(|&(_, x)| *x == max)
+            .map(|(i, _)| i)
+            .unwrap()
+            + start;
+        pos_list.push(pos);
     }
-    10 * max + sec
+
+    let mut value: u64 = 0;
+    for x in pos_list {
+        value = 10 * value + jolts[x] as u64;
+    }
+    value
 }
 
 pub fn solution() {
     let mut sum = 0;
     if let Ok(lines) = read_lines("./input/day3/input") {
         for (i, line) in lines.map_while(Result::ok).into_iter().enumerate() {
-            sum += get_jolt(&line);
-            println!("bank {}: {} jolt {}", i, line, get_jolt(&line));
+            let jolt = get_jolt(&line);
+            sum += jolt;
+            println!("bank {}: {} jolt {}", i, line, jolt);
         }
     }
     println!("sum {}", sum);
